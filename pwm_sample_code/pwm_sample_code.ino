6a3f35adff689ba101b5a95df80258ff11ce57b9
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-04-06 13:48:21 -0400

    Motors moving at the same speed and direction

--- pwm_sample_code/pwm_sample_code.ino
+++ pwm_sample_code/pwm_sample_code.ino
@@ -2,15 +2,12 @@ void setup() {
   // put your setup code here, to run once:
   analogWriteFrequency(3,50);      // set the frequency of pin 3 to 50Hz
   analogWriteFrequency(4,50);      // set the frequency of pin 3 to 50Hz
-  analogWriteResolution(10);       // set the resolution to 10 bits
+  analogWriteResolution(8);       // set the resolution to 10 bits
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(3,700);             // set the top to 700 out of 1023 on pin 3
-  for(int i=0;i <= 1022; i++)     // for loop changing the duty cycle on pin 4
-  {
-    analogWrite(4,i);
-    delay(10);
+  analogWrite(3,15);
+  analogWrite(4,23);
   }
-}
+
