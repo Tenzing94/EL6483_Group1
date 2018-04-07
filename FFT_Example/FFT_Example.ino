0ea98dcd40672ad4c8ea37a5783d4690acf09c38
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-04-07 15:38:56 -0400

    Update on the FFT example

--- FFT_Example/FFT_Example.ino
+++ FFT_Example/FFT_Example.ino
@@ -5,8 +5,8 @@ arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 These values can be changed in order to evaluate the functions
 */
 #define CHANNEL A0
-const uint16_t samples = 64; //This value MUST ALWAYS be a power of 2
-const double samplingFrequency = 5000; //Hz, must be less than 10000 due to ADC
+const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 40000; //Hz, must be less than 10000 due to ADC
 
 unsigned int sampling_period_us;
 unsigned long microseconds;
