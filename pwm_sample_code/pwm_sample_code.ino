ec7907c3e0a8632e21594ece3f4a344e628648bd
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-04-07 13:57:17 -0400

    update

--- pwm_sample_code/pwm_sample_code.ino
+++ pwm_sample_code/pwm_sample_code.ino
@@ -1,13 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
   analogWriteFrequency(3,50);      // set the frequency of pin 3 to 50Hz
-  analogWriteFrequency(4,50);      // set the frequency of pin 3 to 50Hz
+  analogWriteFrequency(4,50);      // set the frequency of pin 4 to 50Hz
   analogWriteResolution(8);       // set the resolution to 10 bits
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(3,15);
-  analogWrite(4,23);
+  analogWrite(3,14);
+  analogWrite(4,24);
   }
 
