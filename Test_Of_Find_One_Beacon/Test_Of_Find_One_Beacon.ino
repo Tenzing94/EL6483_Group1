75ea0619fd55679ccc2f4661265c58fa52e3ac39
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-04-13 17:27:37 -0400

    I made changes to the Test_Of_Find_One_Beacon file

--- Test_Of_Find_One_Beacon/Test_Of_Find_One_Beacon.ino
+++ Test_Of_Find_One_Beacon/Test_Of_Find_One_Beacon.ino
@@ -1,13 +1,22 @@
-/* Regarding the FFT part of this code, most of it was taken from the FFT_03 Example */
+/* *** FINAL PROJECT ***
+ * COURSE: EL-GY 6483 "Real Time Embedded Systems"
+ * SEMESTER: Spring 2018
+ * GROUP: #1
+ * MEMBERS: -> Deliang Wang
+ *          -> John Lee
+ *          -> Mohammad Abbasi
+ *          -> Tenzing Rabgyal 
+ */
+ 
 #include "arduinoFFT.h"
 
 arduinoFFT FFT = arduinoFFT();
 #define CHANNEL A0
 
 #define SAMPLES 128 // WE HAVE TO SET THIS
-#define NUM_BINS 64 // Number of Bins is always half of Number of Samples
 #define SAMPLING_FREQUENCY 40000 // The sampling frequency has to be ATLEAST 2x larger than the largest signal. Since professor will give us at most 20kHz, the sampling frequency is set to 40kHz.
-
+#define SAMPLE_PERIOD (float) 11
+#define MARGIN (double) 0.0
 
 const uint16_t samples = SAMPLES; // This value MUST ALWAYS be a power of 2
 const double samplingFrequency = SAMPLING_FREQUENCY;
@@ -18,13 +27,20 @@ unsigned long microseconds;
 double vReal[samples];
 double vImag[samples];
 
-int state = 0;
-double previousMyReal = 0;
+int found_maximum = 0;
+int chk = 0;
+int found_direction = 0;
+int turn = 0;
+double prevMyReal = 0;
+int max_value = 0;
+
+
+double temp;
 
 
 void setup() {
   // put your setup code here, to run once:
-  sampling_period_us = round(1000000*(1.0/samplingFrequency));
+  //sampling_period_us = round(1000000*(1.0/samplingFrequency));
   Serial.begin(115200);
 
   analogWriteFrequency(3,50);      // set the frequency of pin 3 to 50Hz
@@ -35,59 +51,106 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  double freqmyReal = 0;
   double myReal = 0;
-  double freq[NUM_BINS] = {0};
-  
 
   /*** Sample the data ****************************************************************/
-  for(int i=0; i<samples; i++)
+  for (int i = 0; i < SAMPLES; i++)
   {
-      microseconds = micros();    //Overflows after around 70 minutes!
-
-      vReal[i] = analogRead(CHANNEL);
-      vImag[i] = 0;
-      while(micros() < (microseconds + sampling_period_us)){
-      }
+    temp = analogRead(CHANNEL);
+    vReal[i] =  ((temp * 3.3) / 1024) - 1.65;
+    vImag[i] = 0;
+    delayMicroseconds(SAMPLE_PERIOD);
   }
   /*************************************************************************************************/
   
 
   /*** Compute the FFT *********************************************************************/
-  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+  // FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
   FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
   FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
   /*************************************************************************************************/
 
+  /*** Print out the values of the bins ************************************************************/
+  /*
+  for (uint16_t k = 0; k < (SAMPLES / 2); k++)
+  {
+    double abscissa;
+    abscissa = ((k * 1.0 * SAMPLING_FREQUENCY) / SAMPLES);
+    Serial.print(abscissa, 2);
+    Serial.print("Hz: ");
+    Serial.println(vReal[k], 4);
+  }
+  Serial.println("");
+  delay(1000);
+*/
+ /**********************************************************************************************/
 
+ 
   myReal = vReal[16]; /* Lets say the first beacon is emmitting a 5kHz Sine wave. The bin with index 16 refers to that */
-  //Serial.println(vReal[16], 4);
-  
-  /*** If myReal is larger than the previousMyReal, then we have to keep turning the robot to find the with the strongest signal. ***/
-  if ((myReal > previousMyReal) && (state == 0))
+
+  // find the direction of the frequency
+  if (chk == 0)
   {
-    state = 0;
+    analogWrite(3,58);
+    analogWrite(4,58);
+    delay(250);
+    chk = 1;
+    Serial.println("1. I'm in the CHECK if statement.");
   }
-  else
+  else if ((found_direction == 0) && (chk == 1))
   {
-    state = 1;
-    previousMyReal = 0;
+    if ((prevMyReal + MARGIN) > myReal)
+    {
+      turn = 2;
+    }
+    else
+    {
+      turn = 1;
+    }
+    found_direction = 1;
+    Serial.println("2. I'm in the FOUND_DIRECTION if statement.");
+  }
+ 
+
+  if ((found_direction == 1) && (found_maximum == 0) && (chk == 1))
+  {
+    if (turn == 1)
+    {
+      analogWrite(3,58);
+      analogWrite(4,58);
+    }
+    else if (turn == 2)
+    {
+      analogWrite(3,95);
+      analogWrite(4,95);
+    }
+    Serial.println("3. I'm in the FINDING THE MAXIMUM if statement.");
+
+    if((prevMyReal + MARGIN) > myReal)
+    {
+      if (turn == 1)
+      {
+        analogWrite(3,95);
+        analogWrite(4,95);
+        delay(250);
+      }
+      else if (turn == 2)
+      {
+        analogWrite(3,58);
+        analogWrite(4,58);
+        delay(250);
+      }
+      found_maximum = 1;
+      Serial.println("4. I'm in the FOUND THE MAXIMUM if statement.");
+    }
   }
 
-  if (state == 0)
-  {
-    analogWrite(3,61);      // state 0, so we have it turning until the previous point is higher than current point
-    analogWrite(4,61);
-    delay(500);
-  }
-  else if (state == 1); // state == 1 means that we have found the direction of the signal, so we move towards it
-  {
+  if ((found_maximum == 1) && (found_direction == 1) && (chk == 1))
+  { //Once the direction of the beacon is found, move straight towards it.
     analogWrite(3,60);
-    analogWrite(4,92);
-    delay(500);
+    analogWrite(4,94);
+    Serial.println("5. I'm in the GOING TOWARDS THE LAST BEACON if statement.");
   }
-
-  previousMyReal = myReal; // Assign the current myReal to the previousMyReal so that that value can be used in the next loop
-  analogWrite(3, 76); // 76 means STOP
-  analogWrite(4, 76); // 76 means STOP 
+  prevMyReal = myReal;
+  Serial.println("*** THIS IS THE END OF THIS LOOP ***");
 }
