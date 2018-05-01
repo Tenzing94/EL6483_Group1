04695f432beb206fe224916af61c7887ac9cc903
Deliang Wang <deliangw923@gmail.com>
2018-04-30 20:35:42 -0400

    tone funciton
    generate chirp with buzzer

---
+++ Buzzer_Test/buzzer_test.ino
@@ -0,0 +1,15 @@
+//A sketch to demonstrate the tone() function
+
+//Specify digital pin on the Arduino that the positive lead of piezo buzzer is attached.
+int piezoPin = 5;
+
+void setup() {
+
+}
+
+void loop() {
+  
+  tone(piezoPin, 10000, 250);
+  delay(750);
+  
+}
