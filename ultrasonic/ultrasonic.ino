43e05cb338b2cbb58da1171fa26d72df102d0db1
CalmSquirrel <jjl624@nyu.edu>
2018-05-02 12:21:05 -0400

    Update ultrasonic.ino

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,24 +1,28 @@
+const int trig = 5;
+const int echo = 6;
 long duration;
 int distance;
 
 void setup() {
   // put your setup code here, to run once:
-  
-  analogWriteFrequency(5,50000);    //Set the period of the pwm to 20us
-  analogWrite(5,512);               //trigger set to be on for 10us and off for 10us
-  Serial.begin(115200);
-
 }
 
 void loop() {
-digitalWrite(5, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(5, HIGH);
-delayMicroseconds(10);
-digitalWrite(5, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(6, HIGH);
+
+ultrasonic();
 
 Serial.println(distance);
 }
+
+void ultrasonic()
+{
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  duration = pulseIn(echo,HIGH);
+  //convert to cm
+  distance = duration * 0.034/2;
