df6bc1e7efd237b7bd44c2cf5d267422acc5aa6a
Tenzing Rabgyal <rabgyaltenzing@gmail.com>
2018-05-04 01:58:41 -0400

    added lots of comments

--- FINALV3_Final_Code/Final_Code_v2/Final_Code_v2.ino
+++ FINALV3_Final_Code/Final_Code_v2/Final_Code_v2.ino
@@ -1,3 +1,8 @@
+/*
+ * GROUP: 1
+ * MEMBERS: TENZING, JOHN, MOHAMMAD, DELIANG
+ */
+ 
 #include "arduinoFFT.h"
 
 arduinoFFT FFT = arduinoFFT();
@@ -7,15 +12,15 @@ arduinoFFT FFT = arduinoFFT();
 #define SAMPLING_FREQUENCY 32000 // The sampling frequency has to be ATLEAST 2x larger than the largest signal.
 #define SIZE_OF_ARRAY 12 
 
-#define TIME_STOPPED_RIGHT_AFTER_MOVING 250
-#define TIME_MOVING_RIGHT_AFTER_DIR_FOUND 800
+#define TIME_STOPPED_RIGHT_AFTER_MOVING 250 
+#define TIME_MOVING_RIGHT_AFTER_DIR_FOUND 800 //After we found direction, how long we move before looking for another direction.
 
 #define TRUE 1
 #define FALSE 0
 
 
 /**************************************CIRCLE SPEED**************************************
- * Size of Array  ---  Tested Speed (NOTE: You may need to play around with this number and tune it)
+ * Size of Array  ---  Tested Speed (NOTE: You may need to play around with this number and tune it. Change it by at most +-15)
  *    8           ---    320
  *   12           ---    255
  *   16           ---    225
@@ -51,26 +56,73 @@ arduinoFFT FFT = arduinoFFT();
  *   40       ---  10000Hz
  *    
  */
- #define FREQ_BIN_INDEX 20
+ #define FREQ_BIN_INDEX 20 // Robot starts by detecting the 5kHz Beacon
 
+ int frequency_bin_index = FREQ_BIN_INDEX; // This variable is incremented by 2 once we reach the current beacon
 
-
+ 
+// Variables for FFT 
 const uint16_t samples = SAMPLES; // This value MUST ALWAYS be a power of 2
 const double samplingFrequency = SAMPLING_FREQUENCY;
 unsigned int sampling_period_us;
 unsigned long microseconds;
 
-double vReal[samples];
+double vReal[samples]; // This array will hold the magnitudes. It is updated by SampleData();
 double vImag[samples];
 
-int frequency_bin_index = FREQ_BIN_INDEX; // Initialized to 5KHz
 
+// Variables for Ultrasonic Sensor
 const int trigPin = 9;
 const int echoPin = 10;
 long duration;
 int distance;
-bool ultrasonicFLAG = FALSE;
 
+// Flag that is set when the Ultrasonic detects an object. 
+int ultrasonicFLAG = FALSE;
+
+
+/********************************** FUNCTION PROTOTYPES **********************************/
+
+// If you only have this in void loop, it will print all the values in serial monitor.
+// If you want to use this function in void loop, comment out everything else in the void loop first.
+void PrintAllValues(); 
+
+// Function that is called in order to update the 'distance' variable.
+void ultrasonic(); 
+
+// Called inside sampleData(). We have disabled it for now.
+void threePointMovingAverage(); 
+
+// Reads, Computes FFT and places the magnitudes in global variable array 'vReal'.
+// This function is called inside PrintAllValues() and read_Signal_One_Sec().
+void sampleData(); 
+
+// This function calls sampleData repeatedly for 1 second.
+// The values are summed together and placed in vReal.
+void read_Signal_One_Sec(); 
+
+// Scans 360 degrees and moves toward the direction of signal.
+void scan_full_circle();
+
+// Scans 5 directions and moves toward the direction of signal.
+void scan_five_directions();
+
+// Scans 3 directions and moves toward the direction of signal.
+// Once we have done a full circle, move forward a bit, then 5 directions, move forward at bit,
+// we have found the general direction. So now we check 3 directions everytime. This goes on
+// till the Ultrasonic sensor detects the beacon.
+// As we move forward closer to the beacon, we might want to make the angle of 3 points smaller.
+// That is why I placed an input paramter on this function. This is so that we can decrease the angle.
+void scan_three_directions(double moving_Time);
+
+// Robot Movements
+void robotCW();
+void robotCCW();
+void robotForward();
+void robotForwardSlow(); // Robot moves forward in a slower speed compared to robotForward()
+void robotStop();
+
+/********************************** VOID SETUP **********************************/
 void setup() {
   sampling_period_us = round(1000000*(1.0/samplingFrequency));
   Serial.begin(115200);
@@ -79,11 +131,14 @@ void setup() {
   analogWriteFrequency(4,50);      // set the frequency of pin 4 to 50Hz // *** White Wire ***
   analogWriteResolution(10);       // set the resolution to 10 bits
 
+  // For Ultrasonic Sensor
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   
 }
 
+
+/********************************** VOID LOOP **********************************/
 void loop() {
   
   //PrintAllValues();
@@ -93,35 +148,45 @@ void loop() {
 
   delay(1000); // Solves the problem of the for loop below skipping the first iteration when the board is reset
 
-  scan_full_circle();
+  scan_full_circle(); // Find the general direction of the signal.
 
+  // After the general direction is found, stop for a bit.
   robotStop();
   delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
 
-  ultrasonic();
+  ultrasonic(); // Call ultrasonic() function once. This function will update the 'distance' global variable
   counter1 = millis();
   counter2 = 0;
-  
+
+  // As long as we do not detect anything while we are moving 
+  // AND
+  // We have moved less than value specified by TIME_MOVING_RIGHT_AFTER_DIR_FOUND macro.
   while(distance > 15 && ((counter2 - counter1) < (TIME_MOVING_RIGHT_AFTER_DIR_FOUND)))
   {
     robotForwardSlow();
     ultrasonic();
     Serial.println(distance);
     counter2 = millis();
-    if (distance <= 15)
+    if (distance <= 15) // If somehow we detect the beacon, set the flag.
     {
       ultrasonicFLAG = TRUE;
-    }
+    }   
   }
+  
+  // After moving, stop for a bit.
   robotStop();
   delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
 
+  // If the flag is set in the previous while loop, this if statement is skipped.
   if (ultrasonicFLAG == FALSE)
   {
-    scan_five_points();
+    scan_five_directions(); // Scan 5 directions. 
+
+    // After the direction is found, stop for a bit.
     robotStop();
     delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
-    ultrasonic();
+    
+    ultrasonic(); // Call ultrasonic() function once. This function will update the 'distance' global variable
     counter1 = millis();
     counter2 = 0;
     while(distance > 15 && ((counter2 - counter1) < (TIME_MOVING_RIGHT_AFTER_DIR_FOUND)))
@@ -132,19 +197,30 @@ void loop() {
       counter2 = millis();
       if (distance <= 15)
       {
-        ultrasonicFLAG = TRUE;
+        ultrasonicFLAG = TRUE; // If somehow we detect the beacon, set the flag.
       }
     }
+    // After moving, stop for a bit.
+    robotStop();
+    delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
   }
 
+  // If the flag is set in the previous while loop, this if statement is skipped.
   while (ultrasonicFLAG == FALSE)
   {
+    // Till now, we did a full circle, move a bit, and 5 directions, move a bit.
+    // The Ultrasonic still did not detect anything.
+    // So now we keep doing: take 3 directions, move a bit, 3 directions, move a bit,...
+    // unitl the Ultrasonic detect something in front of it
+    
+    scan_three_directions(CIRCLE_DELAY_IN_MS);
+
+    // After the direction is found, stop for a bit.
     robotStop();
     delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
-    scan_three_points(CIRCLE_DELAY_IN_MS);
-    robotStop();
-    delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
-    ultrasonic();
+
+    
+    ultrasonic(); // Call ultrasonic() function once. This function will update the 'distance' global variable
     counter1 = millis();
     counter2 = 0;
     while(distance > 15 && ((counter2 - counter1) < (TIME_MOVING_RIGHT_AFTER_DIR_FOUND)))
@@ -155,14 +231,17 @@ void loop() {
       counter2 = millis();
       if (distance <= 15)
       {
-        ultrasonicFLAG = TRUE;
+        ultrasonicFLAG = TRUE; // If somehow we detect the beacon, set the flag. This will break the while loop
       }
     }
   }
   robotStop();
   delay(TIME_STOPPED_RIGHT_AFTER_MOVING);
-  ultrasonicFLAG = FALSE;
-  frequency_bin_index = frequency_bin_index + 2;
+
+  // Once we reach this point, it means that the Ultrasonic detected the beacon. 
+  // So now we have to go to the next beacon 
+  ultrasonicFLAG = FALSE; // reset the flag to FALSE
+  frequency_bin_index = frequency_bin_index + 2; // Increment the index to the next beacon. Eg. 5K to 5.5K
   
   
 }
@@ -170,7 +249,10 @@ void loop() {
 
 /********************************** FUNCTIONS **********************************/
 
-void PrintAllValues()
+/////////////////////////////////////////////////////////////////////////////////////////
+// If you only have this in void loop, it will print all the values in serial monitor.
+// If you want to use this function in void loop, comment out everything else in the void loop first.
+void PrintAllValues() 
 {
   int currentTime = 0;
   double vTempReal[samples] = {0};
@@ -202,9 +284,10 @@ void PrintAllValues()
                                     
   delay(100);   
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-
-// This functin is to read from the ultrasonic
+/////////////////////////////////////////////////////////////////////////////////////////
+// Function that is called in order to update the 'distance' variable.
 void ultrasonic()
 {
   duration = 0;
@@ -223,7 +306,10 @@ void ultrasonic()
     distance = duration*0.034/2;
   }
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////
+// Called inside sampleData(). We have disabled it for now.
 void threePointMovingAverage()
 {
   for (int i = 1; i < 63; i++)
@@ -231,8 +317,11 @@ void threePointMovingAverage()
     vReal[i] = ( (vReal[i] + vReal[i - 1] + vReal[i + 1]) / 3);    
   }
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-// This function computes the FFT
+/////////////////////////////////////////////////////////////////////////////////////////
+// Reads, Computes FFT and places the magnitudes in global variable array 'vReal'.
+// This function is called inside PrintAllValues() and read_Signal_One_Sec().
 void sampleData()
 {
   double temp = 0, myReal = 0;
@@ -255,9 +344,11 @@ void sampleData()
   //threePointMovingAverage();
   
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-
-// This function reads the signal for 1 second and return the largest value read
+/////////////////////////////////////////////////////////////////////////////////////////
+// This function calls sampleData repeatedly for 1 second.
+// The values are summed together and placed in vReal.
 void read_Signal_One_Sec()
 {
   int currentTime = 0;
@@ -266,18 +357,22 @@ void read_Signal_One_Sec()
   currentTime = micros();
   while(micros() < (currentTime + 1000000)) // So, this while loop will run for 1 second
   {
-    sampleData();
-    for (int i = 0; i < samples; i++)
+    sampleData(); // Update the vReal global array with recently read magnitudes
+    
+    for (int i = 0; i < samples; i++) // Loop over the vReal and add them to vTempReal array.
     {
-      vTempReal[i] += vReal[i]; ///////////
+      vTempReal[i] += vReal[i]; // vTempReal is will accumulate the vReal data being updated by sampleData().
     }
   }
-  for (int j = 0; j < samples; j++)
+  for (int j = 0; j < samples; j++) // After 1 second is over, move the accumulated values to vReal global array.
   {
     vReal[j] = vTempReal[j];
   }
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////////////
+// Scans 360 degrees and moves toward the direction of signal.
 void scan_full_circle()
 {
   double realArray[SIZE_OF_ARRAY] = {0}; // This is the array where we will put the mag read from the sides
@@ -317,8 +412,11 @@ void scan_full_circle()
     delay(250);
   }
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-void scan_five_points()
+/////////////////////////////////////////////////////////////////////////////////////////
+// Scans 5 directions and moves toward the direction of signal.
+void scan_five_directions()
 {
   delay(500);
   double realArray[5] = {0}; // This is the array where we will put the mag read from the sides
@@ -397,8 +495,16 @@ void scan_five_points()
     }
   }  
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
-void scan_three_points(double moving_Time)
+/////////////////////////////////////////////////////////////////////////////////////////
+// Scans 3 directions and moves toward the direction of signal.
+// Once we have done a full circle, move forward a bit, then 5 directions, move forward at bit,
+// we have found the general direction. So now we check 3 directions everytime. This goes on
+// till the Ultrasonic sensor detects the beacon.
+// As we move forward closer to the beacon, we might want to make the angle of 3 points smaller.
+// That is why I placed an input paramter on this function. This is so that we can decrease the angle.
+void scan_three_directions(double moving_Time)
 {
   delay(500);
   double realArray[3] = {0}; // This is the array where we will put the mag read from the sides
@@ -454,6 +560,7 @@ void scan_three_points(double moving_Time)
     }
   }
 }
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
 // This function moves the robot in ClockWise Direction
@@ -477,6 +584,7 @@ void robotForward()
   analogWrite(4,60);
 }
 
+// This function moves the robot Forward in a slower speed compared to robotForward()
 void robotForwardSlow()
 {
   analogWrite(3,66);
